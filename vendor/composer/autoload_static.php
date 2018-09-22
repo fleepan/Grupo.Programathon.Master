3c23363ebb5e34ad738486c700f7a83b374ef7ee
Jorge Sanchez <jsan18x@gmail.com>
2018-09-22 14:44:32 -0600

    Correcion errores vendor
    Correcion errores vendor proyecto base v3

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -335,6 +335,7 @@ class ComposerStaticInitc0befc152f0b105b39697b5376fc4b2e
         'App\\Http\\Controllers\\Auth\\ResetPasswordController' => __DIR__ . '/../..' . '/app/Http/Controllers/Auth/ResetPasswordController.php',
         'App\\Http\\Controllers\\Auth\\VerificationController' => __DIR__ . '/../..' . '/app/Http/Controllers/Auth/VerificationController.php',
         'App\\Http\\Controllers\\Controller' => __DIR__ . '/../..' . '/app/Http/Controllers/Controller.php',
+        'App\\Http\\Controllers\\HomeController' => __DIR__ . '/../..' . '/app/Http/Controllers/HomeController.php',
         'App\\Http\\Kernel' => __DIR__ . '/../..' . '/app/Http/Kernel.php',
         'App\\Http\\Middleware\\Authenticate' => __DIR__ . '/../..' . '/app/Http/Middleware/Authenticate.php',
         'App\\Http\\Middleware\\CheckForMaintenanceMode' => __DIR__ . '/../..' . '/app/Http/Middleware/CheckForMaintenanceMode.php',
